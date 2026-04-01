310aff05dcde76ef8c53f8947d9468d8bdd09e61
Ryan <ryan.simao72@gmail.com>
2026-03-31 21:33:21 -0300

    FEITO

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿﻿string Mensagem, RespostaMeme;
+Console.Write("Usuário, o que você pensa sobre mim? ");
+Mensagem = Console.ReadLine()!;
+
+RespostaMeme =
+    $"{Mensagem.Remove(10)}... NÃO, PERA.\nTô nervoso";
+
+Console.WriteLine(RespostaMeme);
